29015e67c59531fa509e0bbd65c142a30a864eb2
Richard Beletatti <richard.beletatti@gmail.com>
2022-05-11 11:59:55 -0300

    formatacao String

--- pastaDoMeuRascunho/rascunho.py
+++ pastaDoMeuRascunho/rascunho.py
@@ -1,12 +1,11 @@
 """
-OPERADORES ARITMETICOS
-
-+ ADIÇÃO
-- SUB
-* MULT
-/ DIVISÃO NORMAL
-// DIVISÃO INTEIRA
-** ELEVADO
-% RESTO DA DIVISÃO
-() PRECEDÊNCIA
+FORMATACAO STRING
 """
+nome = "richao"
+idade = 22
+altura = 1.80
+peso = 97
+imc = peso / (altura ** 2)  # ALTURA ELEVADO A DOIS E DIVIDO POR PESO
+
+print(f'{nome} tem {idade} anos de idade e seu imc e {imc:.2f}')  #  :.2f -> ESTA DIZENDO QUANTAS CASA DECIMAL E' PARA TER NA VARIAVEL IMC.
+print('{} tem {} anos de idade e seu imc e {}'.format(nome, idade, imc))
