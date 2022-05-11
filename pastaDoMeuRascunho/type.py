df093e69e981a01cd441eccb62bbdfb41e57f6fe
Richard Beletatti <richard.beletatti@gmail.com>
2022-05-11 13:41:32 -0300

    input e adaptando o input para outro tipos primitivos

---
+++ pastaDoMeuRascunho/type.py
@@ -0,0 +1,7 @@
+nome = input("Qual seu nome ? ")
+idade = input("Qual sua idade ? ")
+
+ano_nascimento = 2022 - int(idade)
+
+print(f'O seu nome e {nome}')
+print(f'nasceu no ano de {ano_nascimento}')
