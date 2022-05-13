e17376b3e03373b2d19c85a6e54b3376adeef105
Richard Beletatti <richard.beletatti@gmail.com>
2022-05-13 16:47:02 -0300

    Desempacotamento de lista

---
+++ pastaDoMeuRascunho/DesempacotamentoDeLista.py
@@ -0,0 +1,12 @@
+'''
+DESEMPACOTAMENTO DE LISTAS E PYTHON
+'''
+
+lista = ['Luiz', 'Joao', 'Maria', 1,2,3,4,5,6,7,8,]
+
+n1, n2, *outra_Lista = lista  #ARMAZENA TUDO QUE VEM DPS DE *
+
+print(n1, n2, outra_Lista)
+
+
+
