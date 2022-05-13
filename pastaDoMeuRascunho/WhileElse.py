b05a34a53fbc83277a9fc12ac34359428aa17291
Richard Beletatti <richard.beletatti@gmail.com>
2022-05-13 11:33:44 -0300

    While mais Else

---
+++ pastaDoMeuRascunho/WhileElse.py
@@ -0,0 +1,12 @@
+'''
+WHILE / ELSE
+'''
+contador = 1
+acumulador = 1
+while contador <= 10:
+    print(contador,acumulador)
+
+    acumulador = acumulador + contador
+    contador += 1
+else:
+    print("Else !!!")
