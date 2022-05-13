7864c361545c65944913679951ca71fb070b6b0b
Richard Beletatti <richard.beletatti@gmail.com>
2022-05-13 09:27:54 -0300

    Fatiando String

---
+++ pastaDoMeuRascunho/FatiadnoString.py
@@ -0,0 +1,19 @@
+"""
+MANIPULANDO STRING
+* String indices
+* Fatiamento de string [inicio:fim:passo]
+* Funcoes built-in len, abs, type, print, etc
+Essas uncoes podem ser usada diretamente em cada tipo.
+"""
+###########################################################
+texto = "Python s2"
+
+nova_String = texto[2:6]
+print(nova_String)
+
+nova_String2 = texto[-9:-3]
+print(nova_String2)
+
+nova_String3 = texto[0::2]
+print(nova_String3)
+###########################################################
