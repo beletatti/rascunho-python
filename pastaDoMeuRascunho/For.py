7cf670f2a61dbf46c438674b676af5107e6c40b1
Richard Beletatti <richard.beletatti@gmail.com>
2022-05-13 13:35:42 -0300

    For

---
+++ pastaDoMeuRascunho/For.py
@@ -0,0 +1,23 @@
+'''
+For in em Python
+Iterando strings com for
+Funcao range(start=0, stop, step=1)
+'''
+###################################
+texto = 'Python'
+
+for letra in texto:
+    print(letra)
+
+###################################
+
+for numero in range(10):
+    print(numero)
+
+###################################
+
+for n in range (0, 100, 8):
+    print(n)
+
+###################################
+
