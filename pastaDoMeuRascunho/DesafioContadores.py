0c672436c95a7b8660bd103cb82ee6d33d0c4962
Richard Beletatti <richard.beletatti@gmail.com>
2022-05-16 10:57:15 -0300

    Desafio Contadores

---
+++ pastaDoMeuRascunho/DesafioContadores.py
@@ -0,0 +1,20 @@
+# total = 10
+#
+# for contador in range(total):
+#     contador = contador + 1
+#     print(contador)
+#
+# print(" ")
+##########################################################
+# contadorInverso = 11
+# totalInverso = 0
+# while contadorInverso >= totalInverso:
+#     contadorInverso = contadorInverso - 1
+#     if contadorInverso == -1:
+#         break
+#     print(contadorInverso)
+
+##########################################################
+
+for p, r in enumerate(range(10, 1, -1)):
+    print(p, r)
